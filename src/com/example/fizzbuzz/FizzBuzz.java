4d60fd4f67131d82a57bae45eebfda9a22ce8b14
MikelG <25775812+mikelga22@users.noreply.github.com>
2021-02-16 12:15:10 +0100

    Add FizzBuzz

---
+++ src/com/example/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,8 @@
+package com.example.fizzbuzz;
+
+public class FizzBuzz {
+
+    public void apply(int i) {
+        System.out.printf("TODO");
+    }
+}
---
+++ src/com/example/fizzbuzz/Main.java
@@ -0,0 +1,8 @@
+package com.example.fizzbuzz;
+
+public class Main {
+
+    public static void main (String [] args){
+        new FizzBuzz().apply(1);
+    }
+}
